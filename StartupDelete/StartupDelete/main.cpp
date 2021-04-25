77981fdde24bc5662d02f7168dd8db05e8dd3f5f
RobinLe <web@robinle.de>
2021-04-25 18:02:38 +0200

    StartupDelete: Removed multiple folders option
    Removed faulty option to clear multiple directories at once (seperator "|" couldn't be used in command line arguments)

--- StartupDelete/StartupDelete/main.cpp
+++ StartupDelete/StartupDelete/main.cpp
@@ -1,9 +1,9 @@
 /*
 
-EMPTY FOLDER CONTENTS
+DELETE ALL OF A FOLDER'S CONTENTS
 
-Call syntax: StartupDelete.exe <Directory 1>|<Directory 2>|<...>
-e.g. StartupDelete.exe C:\TEMP|C:\deleteme
+Call syntax: StartupDelete.exe <Directory>
+e.g. StartupDelete.exe C:\TEMP
 
 */
 
@@ -19,10 +19,11 @@ int WINAPI WinMain(
 	_In_ LPSTR szCmdLine,
 	_In_ int iCmdShow)
 {
-	// get commandline params
-	auto szCmdLineW = GetCommandLineW();
+	const wchar_t* szPath = nullptr;
 
+	// get commandline params
 	{
+		const wchar_t* const szCmdLineW = GetCommandLineW();
 		int iParamCount = 0;
 		const auto pParams = CommandLineToArgvW(szCmdLineW, &iParamCount);
 
@@ -30,63 +31,45 @@ int WINAPI WinMain(
 		if (iParamCount == 1)
 		{
 			MessageBoxA(NULL,
-				"Invalid call to StartupDelete!\n\nUsage:\nStartupDelete.exe <Path 1>|<Path 2>...",
+				"Invalid call to StartupDelete!\n\nUsage:\nStartupDelete.exe <Path>",
 				"StartupDelete", MB_ICONERROR);
 			return 1;
 		}
 
 		// save paths param
-		szCmdLineW = pParams[1];
+		szPath = pParams[1];
 	}
 
 
-	const auto lenParams = wcslen(szCmdLineW);
-
-	// go through paths
-	wchar_t szPath[MAX_PATH + 1]{};
-	size_t index = 0;
-	for (size_t i = 0; i <= lenParams; i++)
+	// length check (must be shorter than MAX_PATH)
+	const size_t len = wcslen(szPath);
+	if (len > MAX_PATH)
 	{
-		if (szCmdLineW[i] == L'|' || i == lenParams) // path separator or end of arguments
-		{
-			// directory doesn't exist
-			if (!std::filesystem::exists(szPath) || !std::filesystem::is_directory(szPath))
-			{
-				const wchar_t szErrorPrefix[] = L"Directory doesn't exist:\n\"";
-				const wchar_t szErrorSuffix[] = L"\"\nIt will be skipped.";
-
-				const size_t len = wcslen(szErrorPrefix) + wcslen(szPath) + wcslen(szErrorSuffix) +
-					1;
-				wchar_t* szError = new wchar_t[len]{};
-
-				wcscat_s(szError, len, szErrorPrefix);
-				wcscat_s(szError, len, szPath);
-				wcscat_s(szError, len, szErrorSuffix);
-
-				MessageBoxW(NULL, szError, L"StartupDelete", MB_ICONERROR);
-
-				delete[] szError;
-
-
-
-				// clear path
-				memset(szPath, 0, (MAX_PATH) * 2);
-				continue;
-			}
-
-			// path exists --> recursuvely delete all files and folders inside
-			for (const auto& entry : std::filesystem::directory_iterator(szPath))
-				std::filesystem::remove_all(entry.path());
-
-			// clear path
-			memset(szPath, 0, (MAX_PATH) * 2);
-		}
-		else // regular character
-		{
-			szPath[index] = szCmdLineW[i];
-			index++;
-		}
+		MessageBoxA(NULL, "Path was too long", "StartupDelete", MB_ICONERROR);
+		return 1;
 	}
+
+	// directory doesn't exist
+	if (!std::filesystem::exists(szPath) || !std::filesystem::is_directory(szPath))
+	{
+		const wchar_t szErrorPrefix[] = L"Directory doesn't exist:\n\"";
+		const wchar_t szErrorSuffix[] = L"\"";
+
+		const size_t len = wcslen(szErrorPrefix) + wcslen(szPath) + wcslen(szErrorSuffix) + 1;
+		wchar_t* szError = new wchar_t[len] {};
+
+		wcscat_s(szError, len, szErrorPrefix);
+		wcscat_s(szError, len, szPath);
+		wcscat_s(szError, len, szErrorSuffix);
+
+		MessageBoxW(NULL, szError, L"StartupDelete", MB_ICONERROR);
+		delete[] szError;
+		return 1;
+	}
+
+	// directory exists --> recursuvely delete all files and folders inside
+	for (const auto& entry : std::filesystem::directory_iterator(szPath))
+		std::filesystem::remove_all(entry.path());
 
 	return 0;
 }
